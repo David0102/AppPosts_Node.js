d00caf73a3badc7e289186de9ce819ccda40eaa1
David_Moura <davidmoura0102@gmail.com>
2022-10-14 01:53:06 -0300

    Criando rotas admin

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const express = require("express")
 const handlebars = require("express-handlebars")
 const bodyParser = require("body-parser")
 const app = express()
+const admin = require("./routes/admin")
 //const mongoose = require("mongoose")
 
 //Configurações
 //Handlebars
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 //BodyParser
 app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 //mongoose
 
 //Rotas
+app.use('/admin', admin)
 
 //Outros
 const PORT = 8081
