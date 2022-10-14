dcb4e11a501afd31c268f1a9ed3fce7de95cab73
David_Moura <davidmoura0102@gmail.com>
2022-10-14 15:36:19 -0300

    Model de categoria

--- app.js
+++ app.js
@@ -5,16 +5,24 @@ const bodyParser = require("body-parser")
 const app = express()
 const admin = require("./routes/admin")
 const path = require("path")
-//const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 //Configurações
 //Handlebars
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
 //BodyParser
 app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
 //mongoose
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost/blogapp").then(() => {
+    console.log("mongoDB conectado...")
+}).catch((erro) => {
+    console.log("Erro ao se conectar ao mongoDB: " + erro)
+})
 
 //public
 app.use(express.static(path.join(__dirname, "public")))
