f9e408a80a7bb284d2175480bf3e030222563a0f
David_Moura <davidmoura0102@gmail.com>
2022-10-16 20:02:18 -0300

    CRUD de postagens criado

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require("mongoose")
 require("../models/Categoria")
 const Categoria = mongoose.model("categorias")
+require("../models/Postagem")
+const Postagem = mongoose.model("postagens")
 
 router.get('/', (req, res) => {
     res.render("admin/index")
@@ -115,5 +117,114 @@ router.post("/categorias/deletar", (req, res) => {
         res.redirect("/admin/categorias")
     })
 })
+
+router.get('/postagens', (req, res) => {
+    Postagem.find().lean().populate("categoria").sort({ date: 'desc' }).then((postagens) => {
+        res.render("admin/postagens", { postagens: postagens })
+    }).catch((erro) => {
+        req.flash("error_msg", "Houve um erro ao listar as postagens!")
+        res.redirect("/admin")
+    })
+})
+
+router.get('/postagens/add', (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render("admin/addpostagem", { categorias: categorias })
+    }).catch((erro) => {
+        req.flash("error_msg", "Erro ao carregar o formulário!")
+        res.redirect("/admin")
+    })
+})
+
+router.post('/postagens/nova', (req, res) => {
+
+    var erros = []
+
+    if (req.body.categoria == "0") {
+        erros.push({ texto: "Categoria inválida, registre uma categoria!" })
+    }
+
+    if (erros.length > 0) {
+        res.render("admin/addpostagem", { erros: erros })
+    } else {
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            slug: req.body.slug,
+            descricao: req.body.descricao,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categoria
+        }
+
+        new Postagem(novaPostagem).save().then(() => {
+            req.flash("success_msg", "Postagem cadastrada com sucesso!")
+            res.redirect("/admin/postagens")
+        }).catch((erro) => {
+            req.flash("error_msg", "Erro ao cadastrar postagem!" + erro)
+            res.redirect("/admin/postagens")
+        })
+    }
+
+})
+
+router.get('/postagens/edit/:id', (req, res) => {
+    Postagem.findOne({ _id: req.params.id }).lean().then((postagem) => {
+
+        Categoria.find().lean().then((categorias) => {
+            res.render('admin/editpostagens', { categorias: categorias, postagem: postagem })
+        }).catch((erro) => {
+            req.flash("error_msg", "Erro ao listar as categorias!")
+            res.redirect("/admin/postagens")
+        })
+
+    }).catch((err) => {
+        req.flash("error_msg", "Esta postagem não existe")
+        res.redirect("/admin/postagens")
+    })
+})
+
+router.post('/postagens/edit', (req, res) => {
+
+    var erros = []
+
+    if (req.body.categoria == "0") {
+        erros.push({ texto: "Categoria inválida, registre uma categoria!" })
+    }
+
+    if (erros.length > 0) {
+        res.render("admin/editpostagens", { erros: erros })
+    } else {
+
+        Postagem.findOne({ _id: req.body.id }).then((postagem) => {
+            postagem.titulo = req.body.titulo
+            postagem.slug = req.body.slug
+            postagem.descricao = req.body.titulo
+            postagem.conteudo = req.body.conteudo
+            postagem.categoria = req.body.categoria
+
+            postagem.save().then(() => {
+                req.flash("success_msg", "Postagem editada com sucesso!")
+                res.redirect("/admin/postagens")
+            }).catch((erro) => {
+                req.flash("error_msg", "Erro interno ao editar postagem!")
+            })
+
+        }).catch((erro) => {
+            req.flash("error_msg", "Erro ao editar postagem!")
+        })
+
+    }
+
+})
+
+router.get('/postagens/deletar/:id', (req, res) => {
+    Postagem.remove({ _id: req.params.id }).then(() => {
+        req.flash("success_msg", "Postagem deletada com sucesso!")
+        res.redirect('/admin/postagens')
+    }).catch((erro) => {
+        req.flash("error_msg", "Erro ao deletar postagem!")
+        res.redirect('/admin/postagens')
+    })
+})
+
 
 module.exports = router
