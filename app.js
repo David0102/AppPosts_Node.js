245c3c5dabf605bfc6b0be2417373c0e5051db36
David_Moura <davidmoura0102@gmail.com>
2022-10-17 12:36:06 -0300

    Criando o Home Page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require("path")
 const mongoose = require("mongoose")
 const session = require("express-session")
 const flash = require("connect-flash")
+require("./models/Postagem")
+const Postagem = mongoose.model("postagens")
 
 //Configurações
 //Session
@@ -45,6 +47,19 @@ mongoose.connect("mongodb://localhost/blogapp").then(() => {
 app.use(express.static(path.join(__dirname, "public")))
 
 //Rotas
+app.get('/', (req, res) => {
+    Postagem.find().lean().populate("categoria").sort({ date: 'desc' }).then((postagem) => {
+        res.render("index", { postagem: postagem })
+    }).catch((erro) => {
+        req.flash("error_msg", "Houve um erro ao listar as postagens!")
+        res.redirect("/404")
+    })
+})
+
+app.get('/404', (req, res) => {
+    res.send("Erro 404!")
+})
+
 app.use('/admin', admin)
 
 //Outros
