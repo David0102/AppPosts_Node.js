698bbf6024de865af9d951ea601cafe3a505335d
David_Moura <davidmoura0102@gmail.com>
2022-10-18 16:58:29 -0300

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production") {
-    module.exports = { mongoURI: "mongodb+srv://david:root01021020@blogapp.ijfsjkq.mongodb.net/?retryWrites=true&w=majority" }
+    module.exports = { mongoURI: "mongodb+srv://david:mongo1234@blogapp.ijfsjkq.mongodb.net/?retryWrites=true&w=majority" }
 } else {
     module.exports = { mongoURI: "mongodb://localhost/blogapp" }
 }
