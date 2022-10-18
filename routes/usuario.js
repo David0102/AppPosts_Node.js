79e0e79081b57b6806a18854e8048eb937fe26d5
David_Moura <davidmoura0102@gmail.com>
2022-10-18 01:19:43 -0300

    Login e Autenticação criados

--- routes/usuario.js
+++ routes/usuario.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose")
 const bcrypt = require("bcryptjs")
 require("../models/Usuario")
 const Usuario = mongoose.model("usuarios")
+const passport = require("passport")
 
 router.get("/registro", (req, res) => {
     res.render("usuarios/registro")
@@ -76,6 +77,16 @@ router.post("/registro", (req, res) => {
     }
 })
 
+router.get('/login', (req, res) => {
+    res.render("usuarios/login")
+})
 
+router.post('/login', (req, res, next) => {
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/usuarios/login",
+        failureFlash: true
+    })(req, res, next)
+})
 
 module.exports = router
